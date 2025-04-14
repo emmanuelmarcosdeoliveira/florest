7f5c5de72bb0f96fc7dffac0b9bf71c27e123570
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-04-13 21:33:43 -0300

    chore:  created the climate content

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-
+  <!-- Conteúdo do dia e Clima -->
+    <div class="container flex justify-center md:justify-start mt-4">
+      <div class="bg-verde-900 inline-flex items-center rounded-md">
+        <span class="-left-1 bg-verde-200 block relative rounded size-2"></span>
+        <div class="divide-verde-800 divide-x-2 flex gap-2 text-verde-300">
+          <div id="day" class="capitalize px-4 py-2">segunda</div>
+          <div id="temperature" class="px-4 py-2">25°</div>
+          <div id="climate" class="px-4 py-2">⛅</div>
+        </div>
+      </div>
+    </div>
 `;
