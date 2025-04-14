acaf25d6498646b75e3a506df7cf842d781b023c
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-04-14 19:07:05 -0300

    chore:  developed the introduction component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import logo from "./assets/forest.svg";
-
+import wildbeaast from "./assets/parceiros/wildbeast.svg";
+import floresta from "./assets/floresta.jpg"
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <!-- Conteúdo do dia e Clima -->
     <div class="container flex justify-center md:justify-start mt-4">
@@ -44,4 +45,28 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
         </ul>
       </nav>
     </header>
+      <!--Introdução -->
+<section class="container">
+  <div class="text-white pt-64 px-8 pb-8 relative rounded-2xl overflow-hidden bg-gradient-to-t from-verde-950/80 ">
+      <img class="absolute size-full object-cover inset-0  -z-10" src=${floresta} alt="Imagem de floresta">
+      <div class="bg-verde-950 inline-flex items-center gap-8 mb-8 py-1 pr-1 rounded-full pl-4">
+        Vagas para Dezembro abertas
+        <a class="bg-verde-400 text-verde-900 px-4 py-2 inline-block rounded-full"   href="">Reserve Hoje</a>
+
+
+     </div>
+      <h1 class=" mb-20 font-serif mb-8 text-balance text-5xl max-w-screen-sm ">Venha Experimentar a Vida na Floresta</h1>
+     <div class="text-sm flex justify-between items-end">
+      <p>Melhores lugares para visitar <a class="underline decoration-2 underline-offset-4 hover:no-underline " href="">2049</a> </p>
+      <div >
+        <p class="uppercase mb-2">Recomendado por </p>
+        <p class="flex items-center gap-4">
+          <img class="w-28" src=${wildbeaast} alt="logo Wildbeast">
+          <span>|</span>
+          Revista Nacional
+        </p>
+      </div>
+    </div>
+      </div>
+</section>
 `;
