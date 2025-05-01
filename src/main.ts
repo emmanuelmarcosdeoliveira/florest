eda25fc48a6d24dbee3b019044a7e65079b1bfd9
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-01 08:44:25 -0300

    styles: developed the experiences section

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import canoagem from "./assets/canoagem.jpg";
 import floresta from "./assets/floresta.jpg";
 import logo from "./assets/forest.svg";
+import observacao_noturna from "./assets/observacao-noturna.jpg";
 import wildbeaast from "./assets/parceiros/wildbeast.svg";
+import vida_selvagem from "./assets/vida-selvagem.jpg";
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <!-- Conteúdo do dia e Clima -->
     <div class="container flex justify-center md:justify-start mt-4">
@@ -138,6 +142,154 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
             class="bg-verde-950/60 justify-self-end m-2 px-4 py-2 rounded-full self-start text-sm/none text-white uppercase"
             >Shapire</span
           >
+        </div>
+      </div>
+    </section>
+    <!-- Eventos -->
+    <section class="bg-verde-900 pt-12 py-12 sm:pb-16 sm:py-16" id="eventos">
+      <div class="container">
+        <p
+          class="mb-4 mx-auto text-center text-sm text-verde-200 tracking-widest uppercase"
+        >
+          conecte-se com a natureza
+        </p>
+        <h2
+          class="font-serif max-w-screen-md mb-8 mx-auto sm:text-6xl text-4xl text-balance text-center text-white"
+        >
+          Cada Som, Cada Momento, Uma Nova Descoberta
+        </h2>
+        <div class="gap-4 grid grid-cols-[repeat(3,minmax(300px,1fr))] overflow-x-auto pb-4 sm:gap-8 snap-mandatory snap-x">
+          <div class="bg-verde-800 gap-4 grid p-6 rounded-xl sm:p-8 snap-center">
+            <h3 class="font-serif text-2xl text-verde-300">Lua Nova</h3>
+            <div class="text-white">
+              <p class="font-serif text-5xl">23</p>
+              <p class="text-xl">Março 2049</p>
+            </div>
+            <p class="text-balance text-gray-300">
+              Melhores períodos para observação astronômica. O céu estará claro
+              e as estrelas visíveis
+            </p>
+            <a
+              class="bg-verde-400 inline-block justify-self-start px-4 py-2 rounded-full self-end text-verde-900"
+              href="#contato"
+              >Reservar 23/03
+            </a>
+          </div>
+        
+        <div>
+          <div class="bg-verde-800 gap-4 grid p-6 rounded-xl sm:p-8 snap-center">
+            <h3 class="font-serif text-2xl text-verde-300">Aurora Boreal</h3>
+            <div class="text-white">
+              <p class="font-serif text-5xl">15</p>
+              <p class="text-xl">Abril 2049</p>
+            </div>
+            <p class="text-balance text-gray-300">
+              Melhores períodos para observação da Aurora Boreal, o céu estará 
+              iluminado pelas estrelas.
+            </p>
+            <a
+              class="bg-verde-400 inline-block justify-self-start px-4 py-2 rounded-full self-end text-verde-900"
+              href="#contato"
+              >Reservar 15/04
+            </a>
+          </div>
+        </div>
+        <div>
+          <div class="bg-verde-800 gap-4 grid p-6 rounded-xl sm:p-8 snap-center">
+            <h3 class="font-serif text-2xl text-verde-300">Chuvas de Meteoros</h3>
+            <div class="text-white">
+              <p class="font-serif text-5xl">18</p>
+              <p class="text-xl">Agosto 2049</p>
+            </div>
+            <p class="text-balance text-gray-300">
+              Melhores períodos para observação da Chuva de Meteoros, o céu estará iluminado pelas estrelas.
+            </p>
+            <a
+              class="bg-verde-400 inline-block justify-self-start px-4 py-2 rounded-full self-end text-verde-900"
+              href="#contato"
+              >Reservar 18/08
+            </a>
+          </div>
+        </div>
+      </div>
+ </section>
+ <!-- Experiences -->
+    <section
+      id="experiencias"
+      class="container gap-8 grid pb-12 sm:grid-cols-2 sm:pt-16"
+    >
+      <div class="bg-verde-900 p-6 rounded-xl sm:p-8">
+        <p class="mb-4 text-sm text-verde-200 tracking-widest uppercase">
+          Experiências exclusivas
+        </p>
+        <h2
+          class="capitalize font-serif lg:text-5xl mb-8 sm:text-4xl text-3xl text-balance text-white"
+        >
+          Escolha a sua proxima aventura
+        </h2>
+        <h3
+          class="before:bg-verde-400 before:h-0.5 before:transition-[width] before:w-6 duration-1000 ease-in-out flex gap-4 hover:before:w-10 items-center text-white text-xl"
+        >
+          Observação Noturna
+        </h3>
+        <p class="mb-8 pl-10 text-balance text-gray-400">
+          Explore a vida selvagem em seu habitat natural sob as estrelas.
+        </p>
+        <h3
+          class="before:bg-verde-400 before:h-0.5 before:transition-[width] before:w-6 duration-1000 ease-in-out flex gap-4 hover:before:w-10 items-center text-white text-xl"
+        >
+          Vida Selvagem
+        </h3>
+        <p class="mb-8 pl-10 text-balance text-gray-400">
+          Observe a vida selvagem em seu habitat natural.
+        </p>
+        <h3
+          class="before:bg-verde-400 before:h-0.5 before:transition-[width] before:w-6 duration-1000 ease-in-out flex gap-4 hover:before:w-10 items-center text-white text-xl"
+        >
+          Canoagem
+        </h3>
+        <p class="mb-8 pl-10 text-balance text-gray-400">
+          Conquiste novos horizontes com nossa equipe especializada.
+        </p>
+      </div>
+      <div class="gap-4 grid grid-cols-2">
+        <div class="cursor-pointer group overflow-hidden relative rounded-xl">
+          <img
+            class="group-hover:scale-110 object-cover size-full transition-transform"
+            src="${canoagem}"
+            alt="Praticando canoagem"
+          />
+          <div
+            class="absolute bg-gradient-to-t flex from-verde-950/80 group-hover:opacity-100 inset-0 items-end opacity-0 p-4 transition-opacity"
+          >
+            <span class="text-white">Canoagem</span>
+          </div>
+        </div>
+        <div
+          class="cursor-pointer group overflow-hidden relative rounded-xl row-span-2"
+        >
+          <img
+            class="group-hover:scale-110 object-cover size-full transition-transform"
+            src="${observacao_noturna}"
+            alt="Observação Noturna"
+          />
+          <div
+            class="absolute bg-gradient-to-t flex from-verde-950/80 group-hover:opacity-100 inset-0 items-end opacity-0 p-4 transition-opacity"
+          >
+            <span class="text-white">Observação Noturna</span>
+          </div>
+        </div>
+        <div class="cursor-pointer group overflow-hidden relative rounded-xl">
+          <img
+            class="group-hover:scale-110 object-cover size-full transition-transform"
+            src="${vida_selvagem}"
+            alt="Vida Selvagem"
+          />
+          <div
+            class="absolute bg-gradient-to-t flex from-verde-950/80 group-hover:opacity-100 inset-0 items-end opacity-0 p-4 transition-opacity"
+          >
+            <span class="text-white">Vida Selvagem</span>
+          </div>
         </div>
       </div>
     </section>
