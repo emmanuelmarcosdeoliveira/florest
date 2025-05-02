9d47b8937cdfc2d04351ba038dfb67b525a7926f
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-02 20:52:50 -0300

    style: desenvolvido o estilo para o menu Mobile

--- src/container/Header.ts
+++ src/container/Header.ts
@@ -21,42 +21,56 @@ export function renderHeader(containerId: string) {
   
     <div class="container flex gap-8 items-center justify-between mt-8 py-8">
       <a href="./"> <img src=${logo} alt="Logo Forest" /></a>
-      <nav>
-        <ul class="flex flex-wrap gap-8 text-white text-xl">
+      <!-- hidden -->
+      <nav id="mobile-menu"
+       class="hidden items-center lg:block max-lg:fixed max-lg:inset-0 max-lg:w-full z-40">
+      <div class="backdrop-blur-md bg-verde-950/40 fixed inset-0 lg:hidden"></div>
+        <ul class="lg:flex lg:flex-wrap lg:gap-8 lg:text-xl max-lg:absolute max-lg:divide-white/10 max-lg:divide-y-2 max-lg:p-8 max-lg:w-full max:lg:z-50 text-2xl text-white">
           <li>
             <a
-              class="hover:underline hover:underline-offset-8"
+              class="block lg:hover:underline lg:hover:underline-offset-8 lg:p-0 lg:py-2 max-lg:hover:bg-white/10 p-4"
               href="#acomodacoes"
               >Acomodações</a
             >
           </li>
           <li>
-            <a class="hover:underline hover:underline-offset-8" href="#eventos"
+            <a class="block lg:hover:underline lg:hover:underline-offset-8 lg:p-0 lg:py-2 max-lg:hover:bg-white/10 p-4" href="#eventos"
               >Eventos</a
             >
           </li>
           <li>
             <a
-              class="hover:underline hover:underline-offset-8"
+              class="block lg:hover:underline lg:hover:underline-offset-8 lg:p-0 lg:py-2 max-lg:hover:bg-white/10 p-4"
               href="#experiencias"
               >Experiências</a
             >
           </li>
           <li>
-            <a class="hover:underline hover:underline-offset-8" href="#contato"
+            <a class="block lg:hover:underline lg:hover:underline-offset-8 lg:p-0 lg:py-2 max-lg:hover:bg-white/10 p-4" href="#contato"
               >Contanto</a
             >
           </li>
         </ul>
       </nav>
+      <button id="mobile-button" class="bg-verde-400 flex gap-3 items-center lg:hidden px-4 py-2 rounded-full text-verde-900">Menu
+      <span class="*:bg-verde-800 *:h-0.5 *:rounded-mb flex flex-col h-3 justify-between w-4">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      </button>
+
     </div>
-    
-  
-  
-  
-  
-  
-  
-  
+
   `;
+  const mobileButton = document.getElementById("mobile-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileButton?.addEventListener("click", () => {
+    mobileMenu?.classList.toggle("hidden");
+    mobileMenu?.classList.toggle("flex");
+  });
+  mobileMenu?.addEventListener("click", () => {
+    mobileMenu?.classList.add("hidden");
+    mobileMenu?.classList.remove("flex");
+  });
 }
